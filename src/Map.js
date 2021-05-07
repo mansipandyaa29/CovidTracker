99ceca908632881dc11a02342408a54d0484bc9d
Mansi Pandya <37405707+mansipandyaa29@users.noreply.github.com>
2021-05-07 13:04:21 +0530

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -8,10 +8,9 @@ function Map({ countries, casesType, center, zoom }) {
     <div className="map">
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
-        {showDataOnMap(countries, casesType)}
+        {showDataOnMap(countries, casesType)} #connects to util.js
       </LeafletMap>
     </div>
   );
 }
-
 export default Map;
