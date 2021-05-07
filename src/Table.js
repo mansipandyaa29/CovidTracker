25eb8f4f948c6b45c95581728fc9db7268d1a63f
Mansi Pandya <37405707+mansipandyaa29@users.noreply.github.com>
2021-05-07 13:20:50 +0530

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -7,6 +7,7 @@ function Table({ countries }) {
     <div className="table">
     #go through all of th countries, map through them, for every single country, return the following
       {countries.map((country) => ( 
+    #numeral adds commas
         <tr><td>{country.country}</td><td><strong>{numeral(country.cases).format("0,0")}</strong></td></tr>
       ))}
     </div>
