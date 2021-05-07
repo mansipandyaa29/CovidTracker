d7a1d99eecc75dce518e123b7bd01adcbb852a6d
Mansi Pandya <37405707+mansipandyaa29@users.noreply.github.com>
2021-05-07 11:29:50 +0530

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -5,13 +5,9 @@ import numeral from "numeral";
 function Table({ countries }) {
   return (
     <div className="table">
-      {countries.map((country) => (
-        <tr>
-          <td>{country.country}</td>
-          <td>
-            <strong>{numeral(country.cases).format("0,0")}</strong>
-          </td>
-        </tr>
+    #go through all of th countries, map through them, for every single country, return the following
+      {countries.map((country) => ( 
+        <tr><td>{country.country}</td><td><strong>{numeral(country.cases).format("0,0")}</strong></td></tr>
       ))}
     </div>
   );
